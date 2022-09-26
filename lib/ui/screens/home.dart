91038ecd74aef2c86cdefc68512a95b4319d3088
Charise Walraven <cherri.walraven@gmail.com>
2022-09-26 15:20:26 +0200

    first working version of basic home screen

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -12,22 +12,58 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
     return NewsAppScaffold(
-      body: Center(
-        child: FutureBuilder(
-            future: newsService.getHeadlines(),
-            builder:
-                (BuildContext context, AsyncSnapshot<List<Article>?> snapshot) {
-              Widget child = const LoadingIndicator();
-              if (snapshot.hasData) {
-                List<Article> articles = snapshot.data ?? [];
-                child = NewsArticles(articles: articles);
-              } else if (snapshot.hasError) {
-                // return error widget
-                child = const ErrorMessage();
-              }
-              return child;
-            }),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "Discover",
+              style: textTheme.displaySmall,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                "News from all over the world",
+                style: textTheme.bodySmall,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              padding: const EdgeInsets.all(8.0),
+              child: const TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.search),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                  future: newsService.getHeadlines(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Article>?> snapshot) {
+                    Widget child = const LoadingIndicator();
+                    if (snapshot.hasData) {
+                      List<Article> articles = snapshot.data ?? [];
+                      debugPrint(articles[4].urlToImage);
+                      child = NewsArticles(articles: articles);
+                    } else if (snapshot.hasError) {
+                      // return error widget
+                      child = const ErrorMessage();
+                    }
+                    return child;
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/widgets/news_articles.dart
+++ lib/ui/widgets/news_articles.dart
@@ -1,45 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/core/models/article.dart';
 
-
 class NewsArticles extends StatelessWidget {
-  const NewsArticles({super.key, required this.articles});
+  const NewsArticles({
+    super.key,
+    required this.articles,
+  });
 
   final List<Article> articles;
+  final double articleMargin = 10.0;
 
   @override
   Widget build(BuildContext context) {
+    double maxArticleHeight =
+        (MediaQuery.of(context).size.height / 10) - articleMargin;
+    double maxArticleWidth =
+        (MediaQuery.of(context).size.width / 1.5) - articleMargin;
     return ListView.builder(
+      shrinkWrap: true,
       itemCount: articles.length,
       itemBuilder: (context, index) {
-        return NewsArticle(article: articles[index]);
+        return NewsArticle(
+          article: articles[index],
+          maxArticleHeight: maxArticleHeight,
+          maxArticleWidth: maxArticleWidth,
+        );
       },
     );
   }
 }
 
-
 class NewsArticle extends StatelessWidget {
-  const NewsArticle({super.key, required this.article});
+  const NewsArticle({
+    super.key,
+    required this.article,
+    this.articleMargin = 10.0,
+    required this.maxArticleHeight,
+    required this.maxArticleWidth,
+  });
 
   final Article article;
+  final double articleMargin;
+  final double maxArticleHeight;
+  final double maxArticleWidth;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        children: [
-          Expanded(child: Image.network(article.urlToImage)),
-          Expanded(
-            flex: 4,
-            child: Column(
-              children: [
-                Text(article.title),
-                Text(article.author),
-              ],
+    TextTheme textTheme = Theme.of(context).textTheme;
+    ImageProvider<Object> image;
+    if (article.urlToImage == "") {
+      image = const AssetImage("assets/article-image-placeholder.jpg");
+    } else {
+      image = NetworkImage(article.urlToImage);
+    }
+    return Padding(
+      padding: EdgeInsets.all(articleMargin),
+      child: SizedBox(
+        height: maxArticleHeight,
+        width: maxArticleWidth,
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: maxArticleHeight,
+                width: maxArticleWidth,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(articleMargin),
+                  image: DecorationImage(
+                    image: image,
+                    onError: (exception, stackTrace) => const AssetImage(
+                        "assets/article-image-placeholder.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: articleMargin),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      article.title,
+                      style: textTheme.labelLarge,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      article.author,
+                      style: textTheme.bodySmall?.copyWith(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
