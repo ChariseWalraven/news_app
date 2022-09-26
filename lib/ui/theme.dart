9226aefdc61e13b4ccc49aff8a5504fbfe60c8b6
Charise Walraven <cherri.walraven@gmail.com>
2022-09-26 15:20:02 +0200

    adds theme data

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData newsAppTheme = ThemeData(
   primarySwatch: Colors.amber,
+  fontFamily: GoogleFonts.poppins().fontFamily,
+  textTheme: TextTheme(
+    displaySmall: const TextStyle(
+      letterSpacing: 0.05,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+      fontSize: 36.0,
+    ),
+    bodySmall: TextStyle(
+      color: Colors.grey.shade600,
+    ),
+  ),
 );
